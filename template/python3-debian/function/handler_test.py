ce114b254ca01b218b5ebd35fe54144243469720
Greg Burd <greg.burd@klar.mx>
2022-11-11 11:50:41 -0500

    Keep the layout Alex prefers.

--- template/python3-debian/function/handler_test.py
+++ template/python3-debian/function/handler_test.py
@@ -1,6 +1,6 @@
 from .handler import handle
 
-# Test your code here
+# Test your handler here
 
 # To disable testing, you can set the build_arg `TEST_ENABLED=false` on the CLI or in your stack.yml
 # https://docs.openfaas.com/reference/yaml/#function-build-args-build-args
